76b105553b83d5bf349a0f7bd8f7413c49bfdfe9
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-05-21 10:29:04 +0300

    producer, mesaj gönderildi.

---
+++ Producer.App/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Producer");
+
+var connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri("amqps://cqwvtmkg:SssJl2QvvDifJS1KRHWCKP6nWUKBnIub@toad.rmq.cloudamqp.com/cqwvtmkg")
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+
+var message = "Message 1";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "demo-queue", mandatory: false, null, body);
+
+Console.WriteLine("Mesajlar gönderildi.");
