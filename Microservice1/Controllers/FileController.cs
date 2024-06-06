735b03958c123fb42c2edc245043f58bb199a20e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-06-06 11:46:10 +0300

    7.gün tamamlandı.

---
+++ Microservice1/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        // upload file endpoint
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile(IFormFile? file)
+        {
+            if (file is null || file.Length == 0)
+                return BadRequest("Invalid file");
+
+            //IFileProvider
+            // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/file-providers?view=aspnetcore-8.0
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", file.FileName);
+
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok();
+        }
+    }
+}
