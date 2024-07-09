c7bab6dbf025e20cfa79ccb7c59a01044656be03
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-07-09 10:23:05 +0300

    1.ders tamamlandı.

---
+++ Products.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Products.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+            return Ok(new { isSuccess = true, Id = 10, Name = name });
+        }
+    }
+}
---
+++ Stocks.API/Controllers/StocksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stocks.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StocksController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new { Id = id, Count = 20 });
+        }
+    }
+}
